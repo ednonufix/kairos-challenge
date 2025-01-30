b4193a77cb430acf8b367e86ebf0b2c7b4b97291
Eduardo Noel <enoel.corebsd@gmail.com>
2025-01-31 00:57:14 +0100

    feat: new module config-server
    add initial configuration for config-server module

---
+++ config-server/src/test/java/com/kairosds/challenge/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,14 @@
+package com.kairosds.challenge.configserver;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import org.junit.jupiter.api.Test;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
