a73f88cc3b0fcdd16c34f85635933cb476dc2f9e
Eduardo Noel <enoel.corebsd@gmail.com>
2025-01-31 23:27:06 +0100

    feat: add prices-api module with initial configuration and application setup

---
+++ prices-api/src/test/java/com/kairosds/challenge/pricesapi/PricesApiApplicationTests.java
@@ -0,0 +1,17 @@
+package com.kairosds.challenge.pricesapi;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class PricesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+		assertThat(PricesApiApplication.class).isNotNull();
+	}
+
+}
