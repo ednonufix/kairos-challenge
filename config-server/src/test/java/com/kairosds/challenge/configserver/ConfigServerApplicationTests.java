5ea5d9fee334d137b9370692d56262b1779753df
Eduardo Noel <enoel.corebsd@gmail.com>
2025-01-31 10:11:19 +0100

    test: add integration tests for config server

--- config-server/src/test/java/com/kairosds/challenge/configserver/ConfigServerApplicationTests.java
+++ config-server/src/test/java/com/kairosds/challenge/configserver/ConfigServerApplicationTests.java
@@ -1,14 +1,20 @@
 package com.kairosds.challenge.configserver;
 
-import org.springframework.boot.test.context.SpringBootTest;
-
 import org.junit.jupiter.api.Test;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
 class ConfigServerApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertThat(ConfigServerApplication.class).isNotNull();
+	}
+
+	@Test
+	void applicationStarts() {
+		ConfigServerApplication.main(new String[]{});
+		assertThat(ConfigServerApplication.class).isNotNull();
 	}
 
 }
---
+++ config-server/src/test/java/com/kairosds/challenge/configserver/ConfigServerIntegrationTest.java
@@ -0,0 +1,23 @@
+package com.kairosds.challenge.configserver;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import static com.kairosds.challenge.configserver.ConfigServerExtension.CONFIG_PORT;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(ConfigServerExtension.class)
+@SpringBootTest(properties = {"spring.cloud.config.enabled=true",
+	"spring.config.import=configserver:http://localhost:" + CONFIG_PORT +"/config"})
+class ConfigServerIntegrationTest {
+
+	private @Value("${variable}") String variable;
+
+	@Test
+	void testConfigServer() {
+		assertThat(variable).isEqualTo("value");
+	}
+}
