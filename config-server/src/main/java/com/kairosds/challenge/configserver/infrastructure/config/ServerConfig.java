7786ef7f8bb7f0db1e36dd96cf54726e48266f09
Eduardo Noel <enoel.corebsd@gmail.com>
2025-01-31 01:14:02 +0100

    feat: update config-server
    update config-server with new banner and security settings

--- config-server/src/main/java/com/kairosds/challenge/configserver/infrastructure/config/ServerConfig.java
+++ config-server/src/main/java/com/kairosds/challenge/configserver/infrastructure/config/ServerConfig.java
@@ -1,4 +1,9 @@
 package com.kairosds.challenge.configserver.infrastructure.config;
 
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Configuration;
+
+@EnableConfigServer
+@Configuration
 public class ServerConfig {
 }
