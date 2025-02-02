9eead56a82c254dad50ea80502b939505b07e21e
Eduardo Noel <enoel.corebsd@gmail.com>
2025-02-02 18:15:39 +0100

    test: add OAuth2 configuration to application.yml and update test class with mock beans

--- prices-api/src/test/java/com/kairosds/challenge/pricesapi/PricesApiApplicationTests.java
+++ prices-api/src/test/java/com/kairosds/challenge/pricesapi/PricesApiApplicationTests.java
@@ -1,12 +1,19 @@
 package com.kairosds.challenge.pricesapi;
 
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 import org.junit.jupiter.api.Test;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class PricesApiApplicationTests {
+
+	private @MockitoBean ClientRegistrationRepository clientRegistrationRepository;
+	private @MockitoBean JwtDecoder jwtDecoder;
 
 	@Test
 	void contextLoads() {
