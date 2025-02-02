f93808d7f21b746e3e140ae4c8a83440e37b7c1d
Eduardo Noel <enoel.corebsd@gmail.com>
2025-02-03 00:13:24 +0100

    test: remove unnecessary blank line in LdapServerDaemonTest

--- ldap-server/src/test/java/com/kairosds/challenge/ldapserver/infrastructure/adapter/ldap/LdapServerDaemonTest.java
+++ ldap-server/src/test/java/com/kairosds/challenge/ldapserver/infrastructure/adapter/ldap/LdapServerDaemonTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.mockito.Mockito.when;
 
