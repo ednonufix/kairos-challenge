617ff4a789c823c4b9cc3b7600eed3f1d4f746e4
Eduardo Noel <enoel.corebsd@gmail.com>
2025-02-03 13:11:49 +0100

    style: reintroduce @Hidden annotation in AuthorizedRestUI for OpenAPI documentation

--- prices-api/src/main/java/com/kairosds/challenge/pricesapi/infrastructure/adapter/rest/ui/AuthorizedRestUI.java
+++ prices-api/src/main/java/com/kairosds/challenge/pricesapi/infrastructure/adapter/rest/ui/AuthorizedRestUI.java
@@ -1,7 +1,6 @@
 package com.kairosds.challenge.pricesapi.infrastructure.adapter.rest.ui;
 
 import com.kairosds.challenge.pricesapi.application.query.dto.AuthenticationDetail;
-import io.swagger.v3.oas.annotations.Hidden;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.Hidden;
 
 @Hidden
 @RestController
