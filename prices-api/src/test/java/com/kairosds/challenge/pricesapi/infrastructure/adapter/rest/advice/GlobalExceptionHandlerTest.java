5aca5d34641bdccefbdaf8c84496f1617fd9ec6c
Eduardo Noel <enoel.corebsd@gmail.com>
2025-02-03 15:39:44 +0100

    style: remove unnecessary blank line in GlobalExceptionHandlerTest

--- prices-api/src/test/java/com/kairosds/challenge/pricesapi/infrastructure/adapter/rest/advice/GlobalExceptionHandlerTest.java
+++ prices-api/src/test/java/com/kairosds/challenge/pricesapi/infrastructure/adapter/rest/advice/GlobalExceptionHandlerTest.java
@@ -18,7 +18,6 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
